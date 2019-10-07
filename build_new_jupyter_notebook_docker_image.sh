5974c4dbd1232a4f81d42d6837431153359ba4e6
Bruno Luis Cardoso Novo <brunonovo18@gmail.com>
2019-10-07 15:38:30 -0300

    Adding final dependencies to OpenCV and changing the Build Script

--- build_new_jupyter_notebook_docker_image.sh
+++ build_new_jupyter_notebook_docker_image.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Exit if any command fails
+set -e
+
+# Trying to get the Docker System Version
 DOCKER_VERSION=`docker --version`
 
+# Exit if Docker is not installed or running
 if [[ -z $DOCKER_VERSION ]]; then
     echo -e "\nError: Did you install Docker? Please, verify the installation or if it is really running...\n"
     exit 1
@@ -9,11 +14,14 @@ else
     echo -e "\n$DOCKER_VERSION is running!!"
 fi
 
+# Logging into DockerHub
 echo -e "\nLoggin to DockerHub with an existing account"
 docker login
 
+# Building the new image
 echo -e "\nBuilding new Jupyter Image.."
 docker build -t csbrunonovo/jupyter_notebook_docker .
 
+# Pushing the new image to DockerHub
 echo -e "\nPushing the new Jupyter Image to DockerHub.."
 docker push csbrunonovo/jupyter_notebook_docker:latest
